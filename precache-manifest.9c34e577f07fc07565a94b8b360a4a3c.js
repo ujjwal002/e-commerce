d7f18341bc8a8074a327fbeb7da8b04598be1bec
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-20 10:36:46 +0530

    Updates

---
+++ precache-manifest.9c34e577f07fc07565a94b8b360a4a3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5957593725fea29df0b50919b994e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3afb974fc62c991ebad",
+    "url": "/static/css/main.fa3c9e59.chunk.css"
+  },
+  {
+    "revision": "f1aea8512e6a3e76604e",
+    "url": "/static/js/2.8ef64b7c.chunk.js"
+  },
+  {
+    "revision": "d3afb974fc62c991ebad",
+    "url": "/static/js/main.8a71649c.chunk.js"
+  },
+  {
+    "revision": "9ac2e0cd02d1f96c6fae",
+    "url": "/static/js/runtime-main.c2dfa9a1.js"
+  }
+]);
